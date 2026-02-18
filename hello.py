fb3649415038a1ad3c30cd4772b8ecdf87830207
mikel <psience256@gmail.com>
2026-02-17 19:43:10 -0600

    Begin adding say_day_of_week function (python)

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ def say_hello(name):
     print("Hello, " + name)
 
 say_hello("VS Code")
+
+
+def say_day_of_week(date):
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    return print("Today is " + days[date.weekday()])
